4036d385e27d13e72b0c1959d1fdad516a1109a0
Maria Meir <pigyarx@gmail.com>
2023-08-15 14:13:51 -0400

    inicializando projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Quiz Game</h1>
+      
+    </div>
+  );
+}
+
+export default App
