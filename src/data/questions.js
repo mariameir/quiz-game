3893522ae7cff92fd419fbba4760950be95a7feb
Maria Meir <pigyarx@gmail.com>
2023-08-16 13:14:50 -0400

    elaboorando as questoes do quiz

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,14 +1,47 @@
 const data = [
   {
-    question: "",
+    question: "Qual é o principal objetivo do React.js",
     options: [
-      "",
-      "",
-      "",
-      "",
+      "Gerenciar bancos de dados",
+      "Manipular solicitações HTTP",
+      "Facilitar a criação de interfaces de usuário interativas",
+      "Automatizar tarefas de servidor",
     ],
-    answer: "",
-  }
+    answer: "Facilitar a criação de interfaces de usuário interativas",
+  },
+
+  {
+    question: "Como os componentes são reutilizados no React.js?",
+    options: [
+      "Através de consultas SQL",
+      "Por meio de funções matemáticas",
+      "Utilizando importações e composição de componentes",
+      "Incorporando-as diretamente no HTML",
+    ],
+    answer: "Utilizando importações e composição de componentes",
+  },
+
+  {
+    question:"Qual hook do React é usado para gerenciar o estado em componentes funcionais?",
+    options:[
+      "useState",
+      "useEffect",
+      "useStateEffect",
+      "stateManager",
+    ],
+    answer:"useState",
+  },
+
+  {
+    question:"O que é JSX no contexto do React.js?",
+    options:[
+      "Uma linguagem de marcação utilizada para criar interfaces de usuário",
+      "Uma biblioteca de gerenciamento de estado",
+      "Uma abreviação de 'JavaScript XML'",
+      "Uma estrutura de controle de fluxo de programa",
+    ],
+    answer:"Uma abreviação de 'JavaScript XML'",
+  },
 ];
 
 export default data;
