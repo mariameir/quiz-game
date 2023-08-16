3ca3feaa1a05ea020eab974b874f3dc7889b00c0
Maria Meir <pigyarx@gmail.com>
2023-08-16 17:00:22 -0400

    css app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className='App'>
       <h1>Quiz Game</h1>
-      
+      <Welcome />
     </div>
   );
 }
