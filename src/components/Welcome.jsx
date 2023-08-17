08f408e03f7c008e79dcd3d6f1bf07d262fb17d7
Maria Meir <pigyarx@gmail.com>
2023-08-17 15:52:23 -0400

    index.css

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,14 +5,13 @@ import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
-  console.log(quizState);
   return (
     <div id="welcome">
       <h2>Seja bem vindo(a) ao quiz de React.JS</h2>
       <p>Clique no botão para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({type: "CHANGE_STAGE"})}>Iniciar</button>
       <img src={Quiz} alt="inicio do quiz" />
 
     </div>
