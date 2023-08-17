b09058369399bcc5fb798b92d12de11c0bfbf8b3
Maria Meir <pigyarx@gmail.com>
2023-08-17 15:40:03 -0400

    configurando reducer

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext  } from "react";
 import { QuizContext } from "../context/quiz";
 import Quiz from "../img/quiz.svg";
 
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,9 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions'
+
+const STAGES = ["Start", "Playing", "End"]
+const initialStage={
+  gameStage: STAGES[0],
+  questions
+}
+
+const quizReducer = (state, action) => {
+    console.log(state, action)
+    
+      switch (action.type){
+        case "CHANGE_STAGE":
+          return state;
+
+          default:
+            return state;
+      }    
+}
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children}) =>{
-const value = { name: "Quiz"};
+const value = useReducer(quizReducer, initialStage);
 
   return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 };
